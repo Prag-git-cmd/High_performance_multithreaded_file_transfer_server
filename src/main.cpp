884aae92e8448c7e22f3f0fa77b52db46106cea8
Prag-git-cmd <pragya9032@gmail.com>
2026-09-19 22:57:58 +0530

    Implement worker thread functionality in main.cpp
    Fixed worker function syntax and added thread execution.

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include<thread>
+using namespace std;
 
+void worker{
+  cout<<"Worker thread is running\n";
+}
+
+int main(){
+  cout<<"Main thread is running\n";
+  thread t(worker);
+  t.join();
+  cout<<"Main thread is running\n";
+
+  return 0;
+}
