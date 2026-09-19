50ce9f69ff32a8ac272d18400c0a6f8a6dcf1437
Prag-git-cmd <pragya9032@gmail.com>
2026-09-19 23:34:31 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 #include<thread>
 using namespace std;
 
-void worker(){
-  cout<<"Worker thread is running\n";
+void worker(int id){
+  cout<<"Worker" <<id<<" is running\n";
 }
 
 int main(){
   cout<<"Main thread is running\n";
-  thread t(worker);
-  t.join();
-  cout<<"Main thread is running\n";
+  thread t1(worker,1);
+  thread t2(worker,2);
+  thread t3(worker,3);
+
+  t1.join();
+  t2.join();
+  t3.join();
+
+  cout<<"All thread finished running\n";
 
   return 0;
 }
-
