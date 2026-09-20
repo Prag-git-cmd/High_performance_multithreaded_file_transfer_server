7e0f2c7cc294ddcb6c7556bac12cbfcdb550c6ba
Prag-git-cmd <pragya9032@gmail.com>
2026-09-20 22:52:23 +0530

    Update TCPServer.h

--- include/TCPServer.h
+++ include/TCPServer.h
@@ -1,1 +1,22 @@
+#ifndef TCP_SERVER_H
+#define TCP_SERVER_H
 
+class TCPServer
+{
+public:
+    TCPServer(int port);
+    ~TCPServer();
+
+    bool start();
+    void run();
+
+private:
+    int serverSocket;
+    int port;
+
+    bool createSocket();
+    bool bindSocket();
+    bool listenForConnections();
+};
+
+#endif
