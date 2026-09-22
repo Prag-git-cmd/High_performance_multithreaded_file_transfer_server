a48984e0dd24dec5af94be0695fbacec50341e4c
Prag-git-cmd <pragya9032@gmail.com>
2026-09-22 19:09:55 +0530

    Implement basic TCP server in server_main.cpp

---
+++ src/server_main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "TCPServer.h"
+
+int main()
+{
+    TCPServer server(8080, 3);
+
+    if (!server.start())
+    {
+        std::cerr << "Failed to start server\n";
+        return 1;
+    }
+
+    server.run();
+
+    return 0;
+}
